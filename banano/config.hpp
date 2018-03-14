e657673d93a933d0c9fe073663bd43b58882ff50
renesq <35303112+renesq@users.noreply.github.com>
2018-03-15 00:05:54 +0100

    Livenet rename

--- banano/config.hpp
+++ banano/config.hpp
@@ -9,11 +9,11 @@ namespace rai
 enum class rai_networks
 {
 	// Low work parameters, publicly known genesis key, test IP ports
-	rai_test_network,
+	banano_test_network,
 	// Normal work parameters, secret beta genesis key, beta IP ports
-	rai_beta_network,
+	banano_beta_network,
 	// Normal work parameters, secret live key, live IP ports
-	rai_live_network
+	banano_live_network
 };
 rai::rai_networks const rai_network = rai_networks::ACTIVE_NETWORK;
 std::chrono::milliseconds const transaction_timeout = std::chrono::milliseconds (1000);
